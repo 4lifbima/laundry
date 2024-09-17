5154c8e7d85ce1e4e73b162346932f7b26bda12f
4lifbima <alif67916@gmail.com>
2024-09-17 20:51:47 +0800

    new lagi

---
+++ _footer.php
@@ -0,0 +1,9 @@
+	<footer>
+		<p>Copyright © 2024 | Group 4 Design by Rizqi Setiaji & Azhar Gunawan</p>
+		
+		
+	</footer>
+
+	<script src="<?=url('_assets/js/rumah_laundry.js')?>"></script>
+</body>
+</html>
